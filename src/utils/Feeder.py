56ebc63155e46f32fd734b40c91166226ca68249
JKCTech <jeffreykcorporation@live.nl>
2022-06-17 00:34:37 +0200

    Feeder

--- src/utils/Feeder.py
+++ src/utils/Feeder.py
@@ -1,3 +1,44 @@
+import json
+import requests
+
+from utils.Sysinfo import Sysinfo
+
 class Feeder:
+	ENDPOINT = "https://api.112centraal.nl/v2/"
+
 	def __init__(self):
-		pass
+		sys = Sysinfo()
+
+		self.uuid = sys.getUUID()
+		self.system = sys.system
+		self.version = sys.version
+		self.release = sys.release
+		self.node = sys.node
+		self.machine = sys.machine
+	
+	def feed(self, msgobject):
+		data = {
+			"message": msgobject,
+			"uuid": self.uuid,
+			"system ": self.system,
+			"version": self.version,
+			"release": self.release,
+			"node": self.node,
+			"machine": self.machine
+		}
+
+		headers = {
+			'content-type': 'application/json'
+		}
+
+		try:
+			r = requests.post(
+				self.ENDPOINT,
+				data=json.dumps(data),
+				headers=headers
+			)
+
+			return r
+		
+		except Exception:
+			return False

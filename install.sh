ea0c6f9ba9af38b25541814c6f5611df9accf59c
JKCTech <jeffreykcorporation@live.nl>
2022-07-01 22:11:35 +0200

    Updated install and run scripts

--- install.sh
+++ install.sh
@@ -1,81 +1,55 @@
 #!/bin/bash
 
-# Colors
-RED='\033[0;31m'
-GREEN='\033[0;92m'
-CYAN='\033[0;36m'
-YELLOW='\033[0;33m'
-NC='\033[0m'
-
-# Sexy print function
-print () {
-	if [ $1 == 0 ]; then
-		printf "${GREEN}$2${NC}\n"
-	elif [ $1 == 1 ]; then
-		printf "${RED}$2${NC}\n"
-	elif [ $1 == 2 ]; then
-		printf "${CYAN}$2${NC}\n"
-	elif [ $1 == 3 ]; then
-		printf "${YELLOW}$2${NC}\n"
-	fi
-}
-
-print 0 "Installing Open112..."
+# Install requirements
+apt-get update
+apt-get install -y \
+	git unzip make \
+	libusb-1.0 cmake build-essential libpulse-dev libx11-dev \
+	python3 python3-pip python3-venv
 
 # Temp directory
 mkdir -p .tmp
 rm -rf .tmp/*
 
-# Install requirements
-sudo apt-get update
-sudo apt-get install -y git libusb-1.0 cmake build-essential libpulse-dev libx11-dev
-
-# Install SDR Driver if needed
-if ! command -v rtl_test &> /dev/null
-then
-	print 2 "Installing RTL-SDR Driver..."
-    cd .tmp
-	git clone git://git.osmocom.org/rtl-sdr.git
-	cd rtl-sdr
+# Install RTL-SDR
+install_sdr() {
+	cd .tmp
+	git clone git://git.osmocom.org/RTL-SDR.git
+	cd RTL-SDR
 	mkdir build
 	cd build
 	cmake ../ -DINSTALL_UDEV_RULES=ON
 	make
-	sudo make install
-	sudo ldconfig
-	cd ../../../
-	print 0 "Done!"
-else
-	print 3 "RTL-SDR Driver already installed!"
-fi
+	make install
+	ldconfig
 
-# Select Blacklist
-print 2 "Selecting blacklist..."
-BLACKLIST=/etc/modprobe.d/raspi-blacklist.conf
-if ! cat $BLACKLIST &> /dev/null
-then
-	BLACKLIST=/etc/modprobe.d/blacklist.conf
-fi
-print 0 "Blacklist: $BLACKLIST"
-
-# Blacklist drivers
-DRIVERS="dvb_usb_rtl28xxu rtl2832 rtl2830"
-
-for DRIVER in $DRIVERS
-do
-	if ! cat $BLACKLIST | grep $DRIVER &> /dev/null
+	# Blacklist drivers to prevent kernel from claiming them
+	BLACKLIST=/etc/modprobe.d/raspi-blacklist.conf
+	if ! cat $BLACKLIST &> /dev/null
 	then
-		print 2 "Adding $DRIVER to the blacklist..."
-		echo "blacklist $DRIVER" | sudo tee -a $BLACKLIST
-	else
-		print 3 "$DRIVER already on blacklist."
+		BLACKLIST=/etc/modprobe.d/blacklist.conf
 	fi
-done
 
-# Download & install specific Multimon-NG version
-if ! command -v multimon-ng &> /dev/null
-then
-	print 2 "Installing Multimon-NG..."
+	DRIVERS="dvb_usb_rtl28xxu rtl2832 rtl2830"
+
+	for DRIVER in $DRIVERS
+	do
+		if ! cat $BLACKLIST | grep $DRIVER &> /dev/null
+		then
+			print 2 "Adding $DRIVER to the blacklist..."
+			echo "blacklist $DRIVER" | tee -a $BLACKLIST
+		else
+			print 3 "$DRIVER already on blacklist."
+		fi
+	done
+
+	echo "Installation of RTL-SDR complete!"
+
+	cd ../../../
+}
+
+# Install Multimon
+install_mng() {
 	cd .tmp
 	wget https://github.com/EliasOenal/multimon-ng/archive/1.1.9.zip
 	unzip 1.1.9.zip
@@ -84,14 +58,43 @@ then
 	cd build
 	cmake ../
 	make
-	sudo make install
+	make install
 	cd ../../../
-	print 0 "Done!"
+}
+
+# Check RTL-SDR
+if ! command -v rtl_test &> /dev/null
+then
+	read -p "RTL-SDR not found! Install? " yn
+	case $yn in
+		[Yy]* ) install_sdr; break;;
+		* ) ;;
+	esac
 else
-	print 3 "Multimon-NG already installed!"
+	echo "RTL-SDR already installed!"
 fi
 
-print 2 "Cleaning up..."
-rm -rf .tmp
+# Check Multimon
+if ! command -v multimon-ng &> /dev/null
+then
+	read -p "Multimon-NG not found! Install? " yn
+	case $yn in
+		[Yy]* ) install_mng; break;;
+		* ) ;;
+	esac
+else
+	echo "Multimon-NG already installed!"
+fi
 
-print 0 "Installation complete!"
+# Create python venv
+if [ ! -f "venv/bin/python" ]; then
+	echo "Creating virtual python environment..."
+	python -m pip venv venv
+	source venv/bin/activate
+	echo "Installing / updating packages..."
+	python -m pip install -r requirements.txt
+fi
+
+# Cleanup
+rm -rf .tmp
+echo "Installation complete!"
